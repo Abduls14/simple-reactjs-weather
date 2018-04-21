6c51ac663388a01340f7e2dccc5bda3bf1f13126
Abduls14 <abdul.h@studio14online.co.uk>
2018-04-21 19:51:34 +0100

    Move weather API to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,7 @@ import {
 } from "./types";
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-const ROOT_WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
+const ROOT_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
 const ROOT_LOCATION_URL = "https://freegeoip.net/json/";
 
 export const getCityWeather = city => async dispatch => {
